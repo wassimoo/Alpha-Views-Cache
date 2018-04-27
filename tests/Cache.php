230c157566615e73a5a126f352f01fdcd844d4a0
wassimoo <wassimjbaali5121997@gmail.com>
2018-04-27 04:26:47 +0100

    test cases update

--- tests/Cache.php
+++ tests/Cache.php
@@ -6,7 +6,7 @@
  * 
  * @author Wassim Bougarfa
  * @since 24.04.2018
- * @copyright Alpha-Soltions
+ * @copyright Alpha-Solutions
  * @version 0.1
  * @license BSD http://www.opensource.org/licenses/bsd-license.php
  */
@@ -15,9 +15,32 @@
 
  require __DIR__ . "/../src/ViewCache.php";
 
-try{
-    $cache = new ViewCache("wassimoo", __DIR__ . "/../cache/", ".twig");
-    echo $cache->retrieve("xd");
-}catch(\Exception $ex){
-    echo $ex->getMessage();
+
+/**
+ * @return ViewCache instance or null
+ */
+function create_cache_instance(){
+    $cache = null;
+    try{
+        /** example : every user had his own cache file */
+        $cache = new ViewCache("wassimoo", __DIR__ . "/../cache/", ".cache");
+    }catch(\Exception $ex){
+        echo $ex->getMessage();
+    }
+    return $cache;
 }
+
+
+function retrieve_cache_succefully(ViewCache $cache){
+    echo $cache->retrieve("css");
+}
+
+function register_cache_succefully(ViewCache $cache){
+    $cache->cache("script","Script the World !");
+    $cache->cache("css","CSS the world ! ");
+}
+
+
+$cache = create_cache_instance();
+register_cache_succefully($cache);
+retrieve_cache_succefully($cache);
