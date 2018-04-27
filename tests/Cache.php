41614feb21e595944ce713e8b0ec8999b232037b
wassimoo <wassimjbaali5121997@gmail.com>
2018-04-27 05:05:47 +0100

    multiple cache files use case updated .

--- tests/Cache.php
+++ tests/Cache.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * Alpha View Cache
+ * Alpha Views Cache
  * API Documentation: https://github.com/wassimoo/alpha-view-cache
- * 
+ *
  * @author Wassim Bougarfa
  * @since 24.04.2018
  * @copyright Alpha-Solutions
@@ -11,36 +11,37 @@
  * @license BSD http://www.opensource.org/licenses/bsd-license.php
  */
 
- use AlphaViewCache\ViewCache;
+use AlphaViewCache\ViewCache;
 
- require __DIR__ . "/../src/ViewCache.php";
-
+require __DIR__ . "/../src/ViewCache.php";
 
 /**
+ * Example : every user has his own cache file and though his own views
  * @return ViewCache instance or null
  */
-function create_cache_instance(){
+function create_cache_instance($user)
+{
     $cache = null;
-    try{
-        /** example : every user had his own cache file */
-        $cache = new ViewCache("wassimoo", __DIR__ . "/../cache/", ".cache");
-    }catch(\Exception $ex){
+    try {
+        $cache = new ViewCache($user, __DIR__ . "/../cache/", ".cache");
+    } catch (\Exception $ex) {
         echo $ex->getMessage();
     }
     return $cache;
 }
 
 
-function retrieve_cache_succefully(ViewCache $cache){
-    echo $cache->retrieve("css");
+function register_cache_succefully(ViewCache $cache, $fingerPrint)
+{
+    $cache->cache("head", "$fingerPrint on top of  the World !");
+    $cache->cache("footer", "$fingerPrint in the bottom of the world ! ");
 }
 
-function register_cache_succefully(ViewCache $cache){
-    $cache->cache("script","Script the World !");
-    $cache->cache("css","CSS the world ! ");
-}
+$cache = create_cache_instance("wassim");
+register_cache_succefully($cache, " Alpha");
 
+$cache2 = create_cache_instance("waelo");
+register_cache_succefully($cache2, " Beta");
 
-$cache = create_cache_instance();
-register_cache_succefully($cache);
-retrieve_cache_succefully($cache);
+echo $cache->retrieve("head") . "<br>"; // will load view associated to user (Alpha)
+echo $cache2->retrieve("footer"); // same but for (Beta)
